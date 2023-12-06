a623fd2549824988f1453468d96ee3f83d05fc02
johnmotta <gabriel_motta31@hotmail.com>
2023-12-06 13:46:10 -0300

    Update MainViewController and Add MainView

--- CryptoTracker/Controllers/MainViewController.swift
+++ CryptoTracker/Controllers/MainViewController.swift
@@ -8,5 +8,11 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "mainColor")
+        setupView()
+    }
+    
+    private func setupView() {
+        let screen = MainView()
+        view = screen
     }
 }
