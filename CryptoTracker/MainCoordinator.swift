666e4e123dec676ea9af7fb66ed7db82042e6d34
johnmotta <gabriel_motta31@hotmail.com>
2023-12-07 15:41:56 -0300

    Creation of the Coordinators protocol and implementation of navigation

--- CryptoTracker/MainCoordinator.swift
+++ CryptoTracker/MainCoordinator.swift
@@ -1,3 +1,32 @@
 //
 
-import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    
+    var rootViewController: UINavigationController
+    
+    init(rootViewController: UINavigationController) {
+        self.rootViewController = rootViewController
+    }
+    
+    lazy var mainViewController: MainViewController = {
+        let vc = MainViewController()
+        return vc
+    }()
+    
+    func start() {
+        let viewController = MainViewController()
+        
+        viewController.onLoginTap = {
+            self.showLogin()
+        }
+        
+        self.rootViewController.pushViewController(viewController, animated: true)
+    }
+    
+    private func showLogin() {
+        let coordinator = HomeCoordinator(navigationController: rootViewController)
+        coordinator.start()
+    }
+}
